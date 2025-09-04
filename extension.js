723e76e4bc72df65d9442078ae166ba9fa7c1dca
n0bcode <trantruc499@gmail.com>
2025-09-04 18:39:12 +0700

    refactor: modernize translation and proxy handling
    - Replaced `tunnel` with `http-proxy-agent` for proxy support.
    - Switched to async/await for cleaner `translate` function.
    - Updated translation API to align with library changes.
    - Selected 'refactor' due to significant code restructuring.

--- extension.js
+++ extension.js
@@ -1,35 +1,27 @@
 const vscode = require("vscode");
 const languages = require("./languages.js");
-const gti = require("@vitalets/google-translate-api");
-const tunnel = require('tunnel');
+const { translate: gti } = require("@vitalets/google-translate-api");
+const { HttpProxyAgent } = require('http-proxy-agent');
 const he = require("he");
 const path = require("path");
 const vscodeLanguageClient = require("vscode-languageclient");
 const humanizeString = require("humanize-string");
 const camelcase = require("camelcase");
 
-function translate(text, options) {
-  const gotopts = {};
-  if (options && options.proxy) {
-    const proxy = {
-      host: options.proxy.host,
-      port: options.proxy.port,
-      headers: {
-        "User-Agent": "Node",
-      },
-    };
-    if (options.proxy.auth) {
-      proxy.proxyAuth = `${options.proxy.auth.username}:${options.proxy.auth.password}`;
-    }
+async function translate(text, options) {
+  const translateOptions = {
+    to: options.to,
+    fetchOptions: {}
+  };
 
-    gotopts.agent = tunnel.httpsOverHttp({
-      proxy: proxy,
-    });
+  if (options && options.proxy) {
+    const proxyUrl = `http://${options.proxy.auth ? `${options.proxy.auth.username}:${options.proxy.auth.password}@` : ''}${options.proxy.host}:${options.proxy.port}`;
+    translateOptions.fetchOptions.agent = new HttpProxyAgent(proxyUrl);
   }
-  return gti(text, options, gotopts).then((res) => {
-    res.data = [res.text];
-    return res;
-  });
+
+  const { text: translatedText } = await gti(text, translateOptions);
+  // The old code expected an object with a 'data' array. We'll mimic that.
+  return { data: [translatedText] };
 }
 
 /**
@@ -101,62 +93,55 @@ function getSelectedLineText(document, selection) {
  * @param {vscode.Selection} selection Selection
  * @returns {Promise.<TranslateRes>}
  */
-function getTranslationPromise(selectedText, selectedLanguage, selection) {
-  return new Promise((resolve, reject) => {
-    const { host, port, username, password } = getProxyConfig();
-    const translationConfiguration = {
-      to: selectedLanguage,
+async function getTranslationPromise(selectedText, selectedLanguage, selection) {
+  const { host, port, username, password } = getProxyConfig();
+  const translationConfiguration = {
+    to: selectedLanguage,
+  };
+  if (!!host) {
+    translationConfiguration.proxy = {
+      host,
+      port: Number(port),
     };
-    if (!!host) {
-      translationConfiguration.proxy = {
-        host,
-        port: Number(port),
+    if (!!username) {
+      translationConfiguration.proxy.auth = {
+        username,
+        password,
       };
-      if (!!username) {
-        translationConfiguration.proxy.auth = {
-          username,
-          password,
+    }
+  }
+
+  try {
+    let res = await translate(selectedText, translationConfiguration);
+
+    if (!!res && !!res.data) {
+      // If google rejects the string it will return the same string as input
+      // We can try to split the string into parts, then translate again. Then return it to a
+      // camel style casing
+      if (res.data[0] === selectedText) {
+        const humanizedRes = await translate(humanizeString(selectedText), translationConfiguration);
+        if (!!humanizedRes && !!humanizedRes.data) {
+          return {
+            selection,
+            translation: camelcase(humanizedRes.data[0]),
+          };
+        } else {
+          throw new Error("Google Translation API issue on fallback");
+        }
+      } else {
+        return {
+          selection,
+          translation: res.data[0],
         };
       }
+    } else {
+      throw new Error("Google Translation API issue");
     }
-    translate(selectedText, translationConfiguration)
-      .then((res) => {
-        if (!!res && !!res.data) {
-          // If google rejects the string it will return the same string as input
-          // We can try to split the string into parts, then translate again. Then return it to a
-          // camel style casing
-          if (res.data[0] === selectedText) {
-            translate(
-              humanizeString(selectedText),
-              translationConfiguration
-            ).then((res) => {
-              if (!!res && !!res.data) {
-                resolve(
-                  /** @type {TranslateRes} */ {
-                    selection,
-                    translation: camelcase(res.data[0]),
-                  }
-                );
-              } else {
-                reject(new Error("Google Translation API issue"));
-              }
-            });
-          } else {
-            resolve(
-              /** @type {TranslateRes} */ {
-                selection,
-                translation: res.data[0],
-              }
-            );
-          }
-        } else {
-          reject(new Error("Google Translation API issue"));
-        }
-      })
-      .catch((e) =>
-        reject(new Error("Google Translation API issue: " + e.message))
-      );
-  });
+  } catch (e) {
+    // The original function returned a Promise.reject, so we'll throw an error
+    // which will be caught by the caller's .catch block.
+    throw new Error("Google Translation API issue: " + e.message);
+  }
 }
 
 /**
--- test/index.js
+++
@@ -1,8 +0,0 @@
-const testRunner = require('vscode/lib/testrunner');
-
-testRunner.configure({
-    ui: 'tdd', 		// the TDD UI is being used in extension.test.js (suite, test, etc.)
-    useColors: true // colored output from test results
-});
-
-module.exports = testRunner;
